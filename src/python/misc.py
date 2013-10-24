d54eaf8bb75f0a2cb2214d18a22e38c31c4a0014
Steven Diamond <stevend2@stanford.edu>
2013-10-23 19:55:03 -0700

    added regularization to LDL

--- src/python/misc.py
+++ src/python/misc.py
@@ -1121,6 +1121,81 @@ def kkt_ldl(G, dims, A, mnl = 0):
 
     return factor
 
+REG_EPS = 1e-9
+
+def kkt_reg_ldl(G, dims, A, mnl = 0):
+    """
+    Solution of KKT equations by a dense LDL factorization of the 
+    3 x 3 system.
+    Regularized to handle low rank systems. 
+    
+    Returns a function that (1) computes the LDL factorization of
+    
+        [ H           A'   GG'*W^{-1} ] 
+        [ A           0    0          ],
+        [ W^{-T}*GG   0   -I          ] 
+    
+    given H, Df, W, where GG = [Df; G], and (2) returns a function for 
+    solving 
+    
+        [ H     A'   GG'   ]   [ ux ]   [ bx ]
+        [ A     0    0     ] * [ uy ] = [ by ].
+        [ GG    0   -W'*W  ]   [ uz ]   [ bz ]
+    
+    H is n x n,  A is p x n, Df is mnl x n, G is N x n where
+    N = dims['l'] + sum(dims['q']) + sum( k**2 for k in dims['s'] ).
+    """
+    
+    p, n = A.size
+    ldK = n + p + mnl + dims['l'] + sum(dims['q']) + sum([ int(k*(k+1)/2)
+        for k in dims['s'] ])
+    K = matrix(0.0, (ldK, ldK))
+    ipiv = matrix(0, (ldK, 1))
+    u = matrix(0.0, (ldK, 1))
+    g = matrix(0.0, (mnl + G.size[0], 1))
+
+    def factor(W, H = None, Df = None):
+
+        blas.scal(0.0, K)
+        if H is not None: K[:n, :n] = H
+        K[n:n+p, :n] = A
+        for k in range(n):
+            if mnl: g[:mnl] = Df[:,k]
+            g[mnl:] = G[:,k]
+            scale(g, W, trans = 'T', inverse = 'I')
+            pack(g, K, dims, mnl, offsety = k*ldK + n + p)
+        K[(ldK+1)*(p+n) :: ldK+1]  = -1.0
+        # Add positive regularization in 1x1 block and negative in 2x2 block.
+        K[0 : (ldK+1)*n : ldK+1]  += REG_EPS
+        K[(ldK+1)*n :: ldK+1]  += -REG_EPS
+        lapack.sytrf(K, ipiv)
+
+        def solve(x, y, z):
+
+            # Solve
+            #
+            #     [ H          A'   GG'*W^{-1} ]   [ ux   ]   [ bx        ]
+            #     [ A          0    0          ] * [ uy   [ = [ by        ]
+            #     [ W^{-T}*GG  0   -I          ]   [ W*uz ]   [ W^{-T}*bz ]
+            #
+            # and return ux, uy, W*uz.
+            #
+            # On entry, x, y, z contain bx, by, bz.  On exit, they contain
+            # the solution ux, uy, W*uz.
+
+            blas.copy(x, u)
+            blas.copy(y, u, offsety = n)
+            scale(z, W, trans = 'T', inverse = 'I') 
+            pack(z, u, dims, mnl, offsety = n + p)
+            lapack.sytrs(K, ipiv, u)
+            blas.copy(u, x, n = n)
+            blas.copy(u, y, offsetx = n, n = p)
+            unpack(u, z, dims, mnl, offsetx = n + p)
+    
+        return solve
+
+    return factor
+
 
 def kkt_ldl2(G, dims, A, mnl = 0):
     """
