22f5343e329a1d6316d2d2b22d6e40cd47df863d
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2019-11-03 09:23:53 +0100

    Fixes #144

--- src/python/coneprog.py
+++ src/python/coneprog.py
@@ -2892,7 +2892,7 @@ def lp(c, G, h, A = None, b = None, kktsolver = None, solver = None, primalstart
         resy0 = max(1.0, blas.nrm2(b))
         resz0 = max(1.0, blas.nrm2(h))
 
-        if solsta in (mosek.solsta.optimal, mosek.solsta.near_optimal):
+        if solsta in (mosek.solsta.optimal, getattr(mosek.solsta,'near_optimal',None)):
             if solsta is mosek.solsta.optimal: status = 'optimal'
             else: status = 'near optimal'
 
@@ -3350,7 +3350,7 @@ def socp(c, Gl = None, hl = None, Gq = None, hq = None, A = None, b = None,
         rh = matrix([ blas.nrm2(hl) ] + [ blas.nrm2(hqk) for hqk in hq ])
         resz0 = max(1.0, blas.nrm2(rh))
 
-        if solsta in (mosek.solsta.optimal, mosek.solsta.near_optimal):
+        if solsta in (mosek.solsta.optimal, getattr(mosek.solsta,'near_optimal')):
             if solsta is mosek.solsta.optimal: status = 'optimal'
             else: status = 'near optimal'
 
@@ -4365,7 +4365,7 @@ def qp(P, q, G = None, h = None, A = None, b = None, solver = None,
         resy0 = max(1.0, blas.nrm2(b))
         resz0 = max(1.0, blas.nrm2(h))
 
-        if solsta in (mosek.solsta.optimal, mosek.solsta.near_optimal):
+        if solsta in (mosek.solsta.optimal, getattr(mosek.solsta,'near_optimal',None)):
             if solsta is mosek.solsta.optimal: status = 'optimal'
             else: status = 'near optimal'
 
